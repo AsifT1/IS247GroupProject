98f08527f616b04ccb8eb9388c22f52e6f935558
AsifT1 <AsifT1@umbc.edu>
2025-06-16 17:01:55 -0400

    Added multiplication class, and fixed simple calculator class for group task 2

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,22 +1,11 @@
 public class SimpleCalculator {
     public static void main(String[] args) {
-
         int a = 10;
         int b = 5;
 
-        add adddata=new add();
-        int result=adddata.add(a,b);
-
-        System.out.println("Addition: " + result);
-        System.out.println("Subtraction: " + subtract(a, b));
-        System.out.println("Multiplication: " + multiply(a, b));
-    }
-
-    public static int subtract(int a, int b) {
-        return a - b;
-    }
-    public static int multiply(int a, int b) {
-        return a * b;
+        System.out.println("Addition: " + add.add(a, b));
+        System.out.println("Subtraction: " + sub.sub(a, b));
+        System.out.println("Multiplication: " + multiply.multiply(a, b));
     }
 
 
