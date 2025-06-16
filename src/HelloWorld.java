1b9a40bede5ae1d6376213d70c6aec3f06a19f07
AsifT1 <AsifT1@umbc.edu>
2025-06-16 18:02:36 -0400

    Added docker for group task 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Docker and Java!");
+        System.out.println("Hello Group 3!docker build --no-cache -t hello-java .");
     }
 }
