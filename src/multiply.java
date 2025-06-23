c584c842d2c67a3c4b0a6180a57c23de28404b69
Anukulan <anukulanm@gmail.com>
2025-06-22 21:10:31 -0400

    Update multiply.java

--- src/multiply.java
+++ src/multiply.java
@@ -1,4 +1,7 @@
 public class multiply {
+
+    /* This method takes two diffrent integers and multiplies them
+    *  */
     public static int multiply(int a, int b) {
         return a * b;
     }
