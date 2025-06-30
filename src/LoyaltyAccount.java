07801421c34692be50b5c84cf3d63b64269e080d
Hala Malik <halamalik@mac.myfiosgateway.com>
2025-06-29 20:49:31 -0400

    Add JavaDoc comments and updated HTML docs for LoyaltyAccount

--- src/LoyaltyAccount.java
+++ src/LoyaltyAccount.java
@@ -1,25 +1,60 @@
+/**
+ * Represents a generic loyalty account that holds user-specific data
+ * and keeps track of loyalty points earned.
+ *
+ * @param <T> the type of user data (e.g., String, custom User class, etc.)
+ */
 public class LoyaltyAccount<T> {
+
     private T userData;
     private int points;
 
+    /**
+     * Constructs a new LoyaltyAccount with the given user data.
+     * Points are initialized to zero.
+     *
+     * @param userData the user-specific data to associate with this account
+     */
     public LoyaltyAccount(T userData) {
         this.userData = userData;
         this.points = 0;
     }
 
+    /**
+     * Adds the specified number of points to the loyalty account.
+     *
+     * @param pts the number of points to add
+     */
     public void addPoints(int pts) {
         points += pts;
     }
 
+    /**
+     * Returns the current number of loyalty points.
+     *
+     * @return the current points balance
+     */
     public int getPoints() {
         return points;
     }
 
+    /**
+     * Returns the user data associated with this account.
+     *
+     * @return the user data of type T
+     */
     public T getUserData() {
         return userData;
     }
 
+    /**
+     * Sets or updates the user data associated with this account.
+     *
+     * @param userData the new user data
+     */
     public void setUserData(T userData) {
         this.userData = userData;
     }
 }
+
+
